3749b6cc9e5af4ceb2563049708da46d7817e003
Ameer Hamza Khetran <hamzaa803@gmail.com>
2023-02-07 04:35:36 +0500

    minor adjustments grid

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 
 export default function HomePage() {
     return (    
-    <Grid bg={'green.200'} rowGap={'1.5'}>
+    <Grid rowGap={'1.5'} maxWidth={'90vw'} mr={'5vw'} ml={'5vw'}>
         <GridItem gridRow={'1/2'} gridColumn={'1/2'} justifySelf={['auto', 'center','center']}>
-            <Box display={'flex'}>
+            <Box display={'flex'} justifyContent={'center'}>
                 <Image alt="Panaverse Logo" src='/images/logo1.png' width={80} height={80}></Image>
                 <Text as='h1' fontSize={'48px'} fontWeight={'extrabold'} ml={['10px','auto','auto']}>Panaverse</Text>
             </Box>
@@ -18,7 +18,7 @@ export default function HomePage() {
             </Text>
         </GridItem>
         <GridItem gridRow={'3/4'} gridColumn={'1/2'} justifySelf={'center'}>
-            <Text textAlign={'center'} maxWidth={['full','full', '1200px']}>
+            <Text textAlign={'center'} maxWidth={['full','full', '1080px']}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Text>
         </GridItem>
